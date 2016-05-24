242ad0d0ae45329c63f9b962012c4748907c7eb7
Duong Vu Thai Cuong <duvuthacu261@gmail.com>
2016-05-24 11:50:18 +0700

    Create Index, Login

---
+++ WebApplication/WebApplication/Controllers/ManufacturersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class ManufacturersController : Controller
+    {
+        MobilePhoneSellingEntities db = new MobilePhoneSellingEntities();
+        // GET: Manufacturers
+        public ActionResult ManufacturersPartial()
+        {
+            return PartialView(db.Suppliers.Take(10).ToList());
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        MobilePhoneSellingEntities db = new MobilePhoneSellingEntities();
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection _form)
+        {
+            string _name = _form["txtName"].ToString();
+            string _password = Encryptor.MD5Hash(_form["txtPassword"].ToString());
+            Account _acc = db.Accounts.SingleOrDefault(x => x.Name == _name && x.Password == _password);
+            if(_acc!=null)
+            {
+                ViewBag.LoginStatus = "Đăng nhập thành công !";
+                Session["Account"] = _acc;
+                ViewBag.AccountName = _acc.Name;
+                return View();
+            }
+            ViewBag.LoginStatus = "Đăng nhập không thành công !";
+            return View();
+        }
+    }
+}
