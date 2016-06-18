b7a37d31104f91f9b05d31a054ad82910dfb4222
Duong Vu Thai Cuong <duvuthacu261@gmail.com>
2016-06-18 17:11:44 +0700

    Filter Products

--- WebApplication/WebApplication/Controllers/ManufacturersController.cs
+++ WebApplication/WebApplication/Controllers/ManufacturersController.cs
@@ -16,50 +16,93 @@ namespace WebApplication.Controllers
             return PartialView(db.Manufacturers.Take(10).ToList());
         }
 
-        public ActionResult ProductsByManufacturerId(string _manuId = "", double minPrice = 0, double maxPrice = 100000000)
+        public ActionResult ProductsByManufacturerId(string _manuId, double minPrice = 0, double maxPrice = 10000)
         {
             Manufacturer _manu = db.Manufacturers.SingleOrDefault(x => x.Id == _manuId);
-            if(_manu == null)
+            string filter = Request.Form["rdFilter"];
+            if (_manu == null)
             {
-                Response.StatusCode = 404;
-                return null;
+                return HttpNotFound();
             }
-            //FilterProducts(minPrice, maxPrice);
+
+            #region Filter Product
+            switch (filter)
+            {
+                case "from100":
+                    minPrice = 0;
+                    maxPrice = 100;
+                    Session["Filter"] = "from100";
+                    break;
+                case "100to300":
+                    minPrice = 100;
+                    maxPrice = 300;
+                    Session["Filter"] = "100to300";
+                    break;
+                case "300to700":
+                    minPrice = 300;
+                    maxPrice = 700;
+                    Session["Filter"] = "300to700";
+                    break;
+                case "700to1000":
+                    minPrice = 700;
+                    maxPrice = 1000;
+                    Session["Filter"] = "700to1000";
+                    break;
+                case "from1000":
+                    minPrice = 1000;
+                    maxPrice = 10000;
+                    Session["Filter"] = "from1000";
+                    break;
+                default:
+                    minPrice = 0;
+                    maxPrice = 10000;
+                    Session["Filter"] = null;
+                    break;
+            }
+            #endregion
+
             List<Product> _lstProduct = db.Products.Where(x => x.ManufacturerId == _manuId && x.Price <= maxPrice && x.Price >= minPrice).OrderBy(x => x.Price).ToList();
             if (_lstProduct != null)
             {
                 return View(_lstProduct);
             }
-            return View();
+            return View(_lstProduct);
         }
 
-        public void FilterProducts(double minPrice, double maxPrice)
+        public void FilerProducts(string filter, double minPrice, double maxPrice)
         {
-            string filter = Request["chkFilter"];
-            if (filter.Equals("chk1"))
+            switch (filter)
             {
-                minPrice = 0;
-                maxPrice = 1000000;
-            }
-            if (filter.Equals("chk1to3"))
-            {
-                minPrice = 1000000;
-                maxPrice = 3000000;
-            }
-            if (filter.Equals("chk3to7"))
-            {
-                minPrice = 3000000;
-                maxPrice = 7000000;
-            }
-            if (filter.Equals("chk7to10"))
-            {
-                minPrice = 7000000;
-                maxPrice = 10000000;
-            }
-            if(filter.Equals("chk10"))
-            {
-                minPrice = 10000000;
-                maxPrice = 100000000;
+                case "from100":
+                    minPrice = 0;
+                    maxPrice = 100;
+                    Session["Filter"] = "from100";
+                    break;
+                case "100to300":
+                    minPrice = 100;
+                    maxPrice = 300;
+                    Session["Filter"] = "100to300";
+                    break;
+                case "300to700":
+                    minPrice = 300;
+                    maxPrice = 700;
+                    Session["Filter"] = "300to700";
+                    break;
+                case "700to1000":
+                    minPrice = 700;
+                    maxPrice = 1000;
+                    Session["Filter"] = "700to1000";
+                    break;
+                case "from1000":
+                    minPrice = 1000;
+                    maxPrice = 10000;
+                    Session["Filter"] = "from1000";
+                    break;
+                default:
+                    minPrice = 0;
+                    maxPrice = 10000;
+                    Session["Filter"] = null;
+                    break;
             }
         }
     }
